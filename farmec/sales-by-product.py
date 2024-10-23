346d0236e1b007ea7a9e99e053f41df66d6506d9
Sean Welch <seanwelch51199@icloud.com>
2024-10-23 21:23:34 +0100

    fix

--- farmec/sales-by-product.py
+++ farmec/sales-by-product.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('../spreadsheets/farmec-sales.csv')
+df = pd.read_csv('~/Coding/data-analysis/spreadsheets/farmec-sales.csv')
 
 top_selling = df.groupby('Stock Code')('Quanitity').sum().sort_values(ascending=format(ascending=False))
 print('Top selling products by quantity')
