8189191a9766169ce8498297b8fdcfc6c42337fd
Sean Welch <seanwelch51199@icloud.com>
2024-10-24 17:34:48 +0100

    got some calls for pandas right

--- farmec/sales-by-product.py
+++ farmec/sales-by-product.py
@@ -4,6 +4,6 @@ from pathlib import Path
 file_path = Path('./spreadsheets/farmec-sales.csv')
 df = pd.read_csv(file_path)
 
-top_selling = df.groupby('Stock Code')('Quanitity').sum().sort_values(ascending=format(ascending=False))
+top_selling = df.groupby('Stock Code')['Quantity'].sum().sort_values(ascending=False)
 print('Top selling products by quantity')
 print(top_selling.head())
