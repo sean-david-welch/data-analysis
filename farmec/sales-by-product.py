9b4dc9b1cdfbc55129ddc40bc78cd478f9542b39
Sean Welch <seanwelch51199@icloud.com>
2024-10-24 17:30:37 +0100

    correct path

--- farmec/sales-by-product.py
+++ farmec/sales-by-product.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from pathlib import Path
 
-file_path = Path('../spreadsheets/farmec-sales.csv')
+file_path = Path('./spreadsheets/farmec-sales.csv')
 df = pd.read_csv(file_path)
 
 top_selling = df.groupby('Stock Code')('Quanitity').sum().sort_values(ascending=format(ascending=False))
