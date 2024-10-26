1cdb13b010d792e0c62d4034506a4a76a881da9a
Sean Welch <seanwelch51199@icloud.com>
2024-10-26 15:41:02 +0100

    main

--- farmec/sales-by-product.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from pathlib import Path
-
-file_path = Path('./spreadsheets/farmec-sales.csv')
-df = pd.read_csv(file_path)
-
-top_selling = df.groupby('Stock Code')['Quantity'].sum().sort_values(ascending=False)
-print('Top selling products by quantity')
-print(top_selling.head())
