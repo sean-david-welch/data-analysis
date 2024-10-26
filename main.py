14aae7044b6ed4a2485736556324f681c40026f6
Sean Welch <seanwelch51199@icloud.com>
2024-10-26 15:49:33 +0100

    got main working

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+import pandas as pd
+from pathlib import Path
+
 
 def main():
-    pass
+    file_path = Path('./spreadsheets/farmec-sales.csv')
+    df = pd.read_csv(file_path)
+
+    top_selling = df.groupby('Stock Code')['Quantity'].sum().sort_values(ascending=False)
+    print('Top selling products by quantity')
+    print(top_selling.head())
+
+
+if __name__ == '__main__':
+    main()
