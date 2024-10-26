0ac2be9ab2f5695344ee70a9ee02e72b68bde83c
Sean Welch <seanwelch51199@icloud.com>
2024-10-26 18:12:48 +0100

    some refactoring

--- main.py
+++ main.py
@@ -28,17 +28,19 @@ class SalesSummary(TypedDict):
     parts_records: int
     gross_machine_sales: float
     gross_part_sales: float
-    top_selling_machines: pd.DataFrame
-    top_selling_parts: pd.DataFrame
+    top_selling_machines: pd.Series
+    top_selling_parts: pd.Series
 
 
 class SalesProcessor:
     machine_threshold: int = 1000
-    columns: list = ['Stock Code', 'Description', 'Quantity', 'Net Amount', 'Tax Amount', 'Gross Amount']
+    numerical_cols: list[str] = [SalesColumns.QUANTITY, SalesColumns.NET_AMOUNT, SalesColumns.TAX_AMOUNT, SalesColumns.GROSS_AMOUNT]
     input_path: Path = Path('./spreadsheets/farmec-sales.csv')
     output_path: Path = Path('./data/')
 
     def __init__(self):
+        self.output_path.mkdir(parents=True, exist_ok=True)
+
         self.df: pd.DataFrame = pd.read_csv(self.input_path, dtype={
             SalesColumns.STOCK_CODE: str,
             SalesColumns.DESCRIPTION: str,
@@ -48,7 +50,16 @@ class SalesProcessor:
             SalesColumns.GROSS_AMOUNT: float,
         })
 
-        if missing_columns := set(self.columns) - set(self.df.columns):
+        required_columns = [
+            SalesColumns.STOCK_CODE,
+            SalesColumns.DESCRIPTION,
+            SalesColumns.QUANTITY,
+            SalesColumns.NET_AMOUNT,
+            SalesColumns.TAX_AMOUNT,
+            SalesColumns.GROSS_AMOUNT
+        ]
+
+        if missing_columns := set(required_columns) - set(self.df.columns):
             raise ValueError(f"Missing required columns: {missing_columns}")
 
     def get_sales(self, is_machinery: bool) -> pd.DataFrame:
@@ -58,12 +69,27 @@ class SalesProcessor:
     def get_top_selling(self, df: pd.DataFrame):
         return df.groupby(SalesColumns.STOCK_CODE)[SalesColumns.QUANTITY].sum().sort_values(ascending=False).head()
 
+    def add_totals(self, df: pd.DataFrame) -> pd.DataFrame:
+        totals: pd.Series = pd.Series({
+            SalesColumns.STOCK_CODE: 'TOTAL',
+            SalesColumns.DESCRIPTION: '',
+        })
+
+        for col in self.numerical_cols:
+            totals[col] = df[col].sum()
+
+        return pd.concat([df, pd.DataFrame([totals])], ignore_index=True)
+
+    def save_to_csv(self, df: pd.DataFrame, filename: str) -> pd.Series:
+        df_with_totals = self.add_totals(df)
+        df_with_totals.to_csv(self.output_path / filename, index=False, float_format='%.2f')
+
     def process(self) -> SalesSummary:
         machinery_sales: pd.DataFrame = self.get_sales(is_machinery=True)
         parts_sales: pd.DataFrame = self.get_sales(is_machinery=False)
 
-        machinery_sales.to_csv(self.output_path / 'machinery_sales.csv', index=False)
-        parts_sales.to_csv(self.output_path / 'parts_sales.csv', index=False)
+        self.save_to_csv(machinery_sales, 'machinery_sales.csv')
+        self.save_to_csv(parts_sales, 'parts_sales.csv')
 
         summary: SalesSummary = SalesSummary(
             total_records=len(self.df),
